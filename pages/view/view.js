2099083f3e64540fc6ad34a3c40b5a64039d30ea
André Luferat <33375030+Luferat@users.noreply.github.com>
2021-10-01 10:57:38 -0300

    view

--- pages/view/view.js
+++ pages/view/view.js
@@ -5,6 +5,12 @@ $(document).ready(runPage);
 
 function runPage() {
 
+    // Obté o ID do artigo da URL
+    const id = location.search.replace('?', '');
+
+    // Obtém o artigo do banco de dados
+    
+
     // Altera o título da página
     setTitle(pageTitle);
 
