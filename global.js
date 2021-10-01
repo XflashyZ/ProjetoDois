60697b3f1b601084edb44ed35f62eb99cd886c65
André Luferat <33375030+Luferat@users.noreply.github.com>
2021-10-01 09:50:23 -0300

    Ajustes

--- global.js
+++ global.js
@@ -3,20 +3,6 @@
 // Nome do site
 var siteName = 'ProjetoDois';
 
-// Conexão com Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyBfWBlHOfE7N3J6K_swDzi2Zz6nSWfjd_4",
-    authDomain: "projetoum-fe477.firebaseapp.com",
-    projectId: "projetoum-fe477",
-    storageBucket: "projetoum-fe477.appspot.com",
-    messagingSenderId: "746077889552",
-    appId: "1:746077889552:web:f6c520f9f6e1983b849695"
-};
-
-// Initializa Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
 // Armazena dados do usuário logado
 var user;
 
